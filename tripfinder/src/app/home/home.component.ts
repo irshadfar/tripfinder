9db3ba2cad9e4faa250886fe16d3494c4a989acc
Irshad Farouq <irshadfarouq@gmail.com>
2024-01-07 16:45:15 +0000

    added trip proposals

--- tripfinder/src/app/home/home.component.ts
+++ tripfinder/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TripData, Weather } from '../models/user.models';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  tripData: TripData = {userId: 0, tripId: 0, location: '', dateTime: ''}
+  weather: Weather = {userId:0, tripId:0, location:'', dateTime:'', weather:''}
+
+  constructor(private userService: UserService){
+
+  }
+
+  ngOnInit(){
+    this.tripData.location = "Paris, France",
+    this.tripData.dateTime = "2024-01-10",
+    this.userService.getTripData(this.tripData).subscribe({
+      next: (data) => {
+        this.weather = data;
+      }
+    })
+  }
 }
